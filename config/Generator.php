76f8fd9eaaa34afb515e5c84e5e74c4b5b5b0b71
ONESYSTEAM <info.onesysteam@gmail.com>
2025-04-03 22:26:38 +0800

    Updated

--- config/Generator.php
+++ config/Generator.php
@@ -15,13 +15,9 @@ Dalira PHP version 2.0.0
 Developed By: Adrian Pol Peligrino
 ";
 
-// Get the project root directory (one level up from the config folder)
 $projectRoot = dirname(__DIR__);
-
-// Get the current project directory name
 $projectName = basename($projectRoot);
 
-// Define the .htaccess content
 $htaccessContent = <<<HTACCESS
 RewriteEngine On
 RewriteBase /$projectName/
@@ -31,7 +27,4 @@ RewriteRule ^([a-zA-Z0-9_-]+)$ index.php?page=$1 [L,QSA]
 
 HTACCESS;
 
-// Write to the .htaccess file in the project root
 file_put_contents($projectRoot . '/.htaccess', $htaccessContent);
-
-echo ".htaccess file generated in $projectRoot with RewriteBase /$projectName/\n";
