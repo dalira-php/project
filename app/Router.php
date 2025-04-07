60404c672220671fa70f9fcae25698e271a9a4c5
ONESYSTEAM <info.onesysteam@gmail.com>
2025-04-07 14:55:42 +0800

    Updated

--- app/Router.php
+++ app/Router.php
@@ -24,13 +24,7 @@ class Router
     public static function run()
     {
         $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $projectFolder = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $projectFolder = rtrim($projectFolder, '/');
-        $pathParts = explode('/', trim($projectFolder, '/'));
-        $projectFolder = '/' . $pathParts[0];
-        if (strpos($requestUri, $projectFolder) === 0) {
-            $requestUri = substr($requestUri, strlen($projectFolder));
-        }
+        
         foreach (self::$routes as $route => $callback) {
             if (preg_match("#^$route$#", $requestUri, $matches)) {
                 $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
@@ -38,6 +32,7 @@ class Router
                 return;
             }
         }
+    
         echo template()->render('Errors/404');
     }
 
