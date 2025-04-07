ce1aa24ddbaae75f3a18d2cc85704387e7d0c6ac
ONESYSTEAM <info.onesysteam@gmail.com>
2025-04-07 10:12:13 +0800

    Updated

--- app/Views/Errors/404.php
+++ app/Views/Errors/404.php
@@ -13,7 +13,7 @@
     <div class="row text-start">
         <div class="col-lg-12">
             <h1 class="display-3 fw-bold">Error 404</h1>
-            <p class="lead pt-3">The resource (view or route) you're trying to access may not exist.</p>
+            <p class="lead pt-3">Sorry, the page you're looking for could not be found. <br class="d-none d-sm-block"> The resource (view or route) you're trying to access may not exist.</p>
         </div>
     </div>
 </div>
